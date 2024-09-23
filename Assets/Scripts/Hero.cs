732b443f81426d38e5995cb66e5d3d91e19e7e4e
pol1np <polinaprotcenko@gmail.com>
2024-09-23 17:38:01 +0300

    Начало скрипта Hero

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float lives = 5;
+    [SerializeField] float jumpForce = 15f;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+     private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = rb.GetComponentInChildren<SpriteRenderer>();
+        
+    }
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce/2, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.8f);
+        isGrounded = collider.Length > 1;
+    }
+}   
