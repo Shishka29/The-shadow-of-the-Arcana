29ec8b4057f99e391c720a7f3e3036dcf559f63b
4319NikolaevaVI <zavraghost@gmail.com>
2025-01-16 09:30:06 +0300

    Летающий враг

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class FlyingEnemy : Entity
+{
+    
+    private SpriteRenderer sprite;
+    [SerializeField] private AIPath aiPath;
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        lives = 2;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.flipX = aiPath.desiredVelocity.x <= 0.01f;
+    }
+}
